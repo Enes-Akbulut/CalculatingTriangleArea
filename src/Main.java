40d39590a4bd830a57b1d3fef929b4e268940d18
Enes Akbulut <107497616+Enes-Akbulut@users.noreply.github.com>
2022-06-22 23:07:39 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner girdi = new Scanner(System.in);
+        double kenar1,kenar2,kenar3,cevresi,u,kok;
+        System.out.print("1. kenari giriniz= ");
+
+        kenar1 =girdi.nextDouble();
+        System.out.print("2. kenari giriniz= ");
+
+        kenar2 =girdi.nextDouble();
+        System.out.print("3. kenari giriniz= ");
+        kenar3 = girdi.nextDouble();
+
+        u=(kenar1+kenar2+kenar3)/2  ;
+        cevresi= 2*u;
+        System.out.println(cevresi);
+        kok=u*(u-kenar1)*(u-kenar2)*(u-kenar3);
+        System.out.println("ucgenin alani = " + Math.sqrt(kok));
+
     }
 }
